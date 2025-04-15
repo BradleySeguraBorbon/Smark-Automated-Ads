b971f5e7316f50a3066c26ba22fc29cf10a12520
SebaVZ <sebasver070@gmail.com>
2025-04-14 19:13:24 -0600

    ADD: AdMessage CRUD operations addition

---
+++ smark/src/api/adMessage/[id]/route.ts
@@ -0,0 +1,111 @@
+import { NextResponse } from 'next/server';
+import mongoose from 'mongoose';
+import { connectDB } from '@/config/db';
+import AdMessages from '@/models/AdMessage';
+
+function getMissingFields(body: any, requiredFields: string[]) {
+    return requiredFields.filter(field => body[field] === undefined || body[field] === null);
+}
+
+function isValidObjectId(id: string) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
+
+export async function GET(request: Request) {
+    try {
+        await connectDB();
+        const { searchParams } = new URL(request.url);
+
+        const id = searchParams.get('id');
+
+        if (!id || !isValidObjectId(id)) {
+            return NextResponse.json({ message: 'Invalid or missing id parameter' }, { status: 400 });
+        }
+
+        const adMessage = await AdMessages.findById(id);
+
+        if (!adMessage) {
+            return NextResponse.json({ message: 'No AdMessages found' }, { status: 404 });
+        }
+
+        return NextResponse.json({
+            message: 'AdMessage found',
+            result: adMessage,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Error getting AdMessage' }, { status: 500 });
+    }
+}
+
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+    try {
+        await connectDB();
+        const { id } = params;
+
+        if (!id || !isValidObjectId(id)) {
+            return NextResponse.json({ message: 'Invalid or missing id parameter' }, { status: 400 });
+        }
+
+        const body = await request.json();
+
+        const requiredFields = [
+            'name', 'MarketingCampaignId',
+            'type', 'status',
+            'content', 'attachments',
+            'templateId', 'sendDate'
+        ];
+
+        const missingFields = requiredFields.filter(field => body[field] === undefined || body[field] === null);
+
+        if (missingFields.length > 0) {
+            return NextResponse.json(
+                { message: `Missing fields: ${missingFields.join(', ')}` },
+                { status: 400 }
+            );
+        }
+
+        if (isNaN(Date.parse(body.sendDate))) {
+            return NextResponse.json({ message: 'Invalid sendDate format' }, { status: 400 });
+        }
+
+        const adMessage = await AdMessages.findByIdAndUpdate(id, body, { new: true, runValidators: true });
+
+        if (!adMessage) {
+            return NextResponse.json({ message: 'AdMessage not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({
+            message: 'AdMessage updated successfully',
+            result: adMessage,
+        });
+    } catch (error: any) {
+        console.error(error);
+        if (error.name === 'ValidationError') {
+            return NextResponse.json({ error: error.message }, { status: 422 });
+        }
+        return NextResponse.json({ error: 'Error updating AdMessage' }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    try {
+        await connectDB();
+        const { id } = params;
+        
+        if (!id || !isValidObjectId(id)) {
+            return NextResponse.json({ message: 'Invalid or missing id parameter' }, { status: 400 });
+        }
+
+        const adMessageDeleted = await AdMessages.findByIdAndDelete(id);
+
+        if (!adMessageDeleted) {
+            return NextResponse.json({ message: 'AdMessage not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: 'AdMessage deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Error deleting AdMessage' }, { status: 500 });
+    }
+}
---
+++ smark/src/api/adMessage/route.ts
@@ -0,0 +1,98 @@
+import { NextResponse } from 'next/server';
+import { connectDB } from '@/config/db';
+import AdMessages from '@/models/AdMessage';
+
+export async function GET(request: Request) {
+    try {
+        await connectDB();
+        const { searchParams } = new URL(request.url);
+
+        const page = parseInt(searchParams.get('page') || '1');
+        const limit = parseInt(searchParams.get('limit') || '5');
+
+        if (isNaN(page) || page < 1 || isNaN(limit) || limit < 1) {
+            return NextResponse.json(
+                { message: 'Invalid parameters: page y limit should be greater than 0.' },
+                { status: 400 }
+            );
+        }
+
+        const skip = (page - 1) * limit;
+
+        const total = await AdMessages.countDocuments();
+        const adMessage = await AdMessages.find().skip(skip).limit(limit);
+
+        if (adMessage.length === 0) {
+            return NextResponse.json({ message: 'No AdMessages found' }, { status: 404 });
+        }        
+
+        const totalPages = Math.ceil(total / limit);
+
+        return NextResponse.json({
+            totalPages,
+            page,
+            limit,
+            result: adMessage,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Error getting AdMessages' }, { status: 500 });
+    }
+
+}
+
+export async function POST(request: Request) {
+
+    try {
+        await connectDB();
+        const body = await request.json();
+
+        const requiredFields = [
+            'name',
+            'MarketingCampaignId',
+            'type',
+            'status',
+            'content',
+            'attachments',
+            'templateId',
+            'sendDate'
+        ];
+
+        const missingFields = requiredFields.filter(field => body[field] === undefined || body[field] === null);
+
+        const { name, marketingCampaignId, type, status, content, attachments, templateId, sendDate } = body;
+
+        if (missingFields.length > 0) {
+            return NextResponse.json(
+                { message: `Missing required fields.`, missingFields },
+                { status: 400 }
+            );
+        }
+
+        if (isNaN(Date.parse(sendDate))) {
+            return NextResponse.json({ message: 'Invalid sendDate format' }, { status: 400 });
+        }
+
+        const adMessage = new AdMessages({
+            name,
+            marketingCampaignId,
+            type,
+            status,
+            content,
+            attachments,
+            templateId,
+            sendDate,
+        });
+
+        await adMessage.save();
+        return NextResponse.json({ message: 'AdMessage created successfully', adMessage },
+            { status: 201 });
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: 'Error creating AdMessage' }, { status: 500 }
+        );
+    }
+
+}
