3f1dd5f5800688270eb1b0ac735aeef78b6b7faa
SebaVZ <sebasver070@gmail.com>
2025-05-20 21:16:36 -0600

    ADD: Swagger Documentation in API endpoint
    - Making Swagger Interface look good in dark mode

--- smark/src/components/NavbarWrapper.tsx
+++ smark/src/components/NavbarWrapper.tsx
@@ -6,7 +6,7 @@ import { Navbar } from './Navbar'
 export function NavbarWrapper() {
     const currentPath = usePathname()
 
-    const hideOnPaths = ['/auth/login', '/auth/register','/auth/reset-password','/auth/email-login']
+    const hideOnPaths = ['/auth/login', '/auth/register','/auth/reset-password','/auth/email-login', '/ajhpi-docs']
     if (hideOnPaths.includes(currentPath)) return null
 
     return <Navbar currentPath={currentPath} />
