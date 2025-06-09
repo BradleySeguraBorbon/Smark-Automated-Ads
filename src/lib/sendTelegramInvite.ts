611861d8169ea39601c594f0b46c84d9e65aa3d9
SebaVZ <sebasver070@gmail.com>
2025-06-09 09:16:06 -0600

    FIX: telegram invitation seems not sent
    - adding try catch statements to manage errors

--- src/lib/sendTelegramInvite.ts
+++ src/lib/sendTelegramInvite.ts
@@ -1,16 +1,21 @@
-import { sendEmail } from "@/lib/utils/mailSender";
+import sendEmail from "@/lib/utils/mailer";
 
 export async function sendTelegramInvite(clientEmail: string, tokenKey: string) {
-    const botUsername = process.env.TELEGRAM_BOT_USERNAME;
-    const link = `https://t.me/${botUsername}?start=${tokenKey}`;
-    const subject = "Activate Telegram Notifications";
+    try {
+        const botUsername = process.env.TELEGRAM_BOT_USERNAME;
+        const link = `https://t.me/${botUsername}?start=${tokenKey}`;
+        const subject = "Activate Telegram Notifications";
 
-    const html = `
-    <p>Hello,</p>
-    <p>Click the link below to activate Telegram notifications with our bot:</p>
-    <p><a href="${link}">${link}</a></p>
-    <p>If you didn't request this, you can ignore this email.</p>
-  `;
+        const html = `
+      <p>Hello,</p>
+      <p>Click the link below to activate Telegram notifications with our bot:</p>
+      <p><a href="${link}">${link}</a></p>
+      <p>If you didn't request this, you can ignore this email.</p>
+    `;
 
-    await sendEmail(clientEmail, subject, html);
+        await sendEmail(clientEmail, subject, html);
+        console.log(`[EMAIL] Telegram invite sent to ${clientEmail}`);
+    } catch (error) {
+        console.error(`[EMAIL] Failed to send Telegram invite to ${clientEmail}:`, error);
+    }
 }
