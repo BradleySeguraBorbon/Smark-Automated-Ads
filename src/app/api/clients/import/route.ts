cfbf6502719ef101f63049f4a1b7a0c768cf6e00
BradleySeguraBorbon <bradleysegura001@gmail.com>
2025-06-16 11:10:13 -0600

    FIX: encryption on import
    - Fix missing encryption on import (which already was implemented, it seems Git merges made it dissapear)

--- src/app/api/clients/import/route.ts
+++ src/app/api/clients/import/route.ts
@@ -3,6 +3,7 @@ import connectDB from "@/config/db";
 import { Clients } from "@/models/models";
 import crypto from "crypto";
 import { sendTelegramInvite } from "@/lib/sendTelegramInvite";
+import { encryptClient } from "@/lib/clientEncryption";
 
 export async function POST(req: NextRequest) {
     try {
@@ -62,7 +63,7 @@ export async function POST(req: NextRequest) {
                 continue;
             }
 
-            const alreadyExists = await Clients.findOne({email});
+            const alreadyExists = await Clients.findOne({ email });
 
             if (alreadyExists) {
                 console.warn(`Duplicate in DB skipped: ${email}`);
@@ -106,7 +107,8 @@ export async function POST(req: NextRequest) {
                 )
             );
 
-            validClients.push(sanitized);
+            const encrypted = encryptClient(sanitized);
+            validClients.push(encrypted);
         }
 
         if (validClients.length === 0) {
